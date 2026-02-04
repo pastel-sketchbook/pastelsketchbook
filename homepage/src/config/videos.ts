b18cf81cf78b6e29e6fd73d4fe407a70dd778441
Pastel Sketchbook <252934749+pastelsketchbook@users.noreply.github.com>
2026-02-04 07:04:24 -0600

    chore(showcase): add 2 new videos (Bocetos de Daejeon, MotherDuck)

--- homepage/src/config/videos.ts
+++ homepage/src/config/videos.ts
@@ -35,7 +35,9 @@ export const VIDEO_CONFIG = {
     'EvcUSPWkOA8',
     'JlPl9MskqJM',
     'drVBXipEOAs',
-    'awa_UWXFOao'
+    'awa_UWXFOao',
+    'nxuaOP3C1Ns',
+    'QLcNRIHYmj0'
   ],
   finance: [
     'K1O4kHjFJaQ',
@@ -50,7 +52,7 @@ export const VIDEO_CONFIG = {
     '0Wtng6Ou3O4',
     '-WYyOwj8EYU'
   ],
-  kubernetes: ['Q4qm1hvVR2A', 'WHonjixQgBY', 'KpSjLQpswW4', '4b-H6ZaoNUE', '_sxPf3tHq3s', '0U-SUJCmKAU', 'VxWyvAO3qb8', 'A4-foXsdQiA', 'C6wLm6NRZW4', 'ctKLD4d146g', 'OnwiaDRj1-k', 'R2zktRqz81U', 'N4nRAfBZ3BE', 'C5HqbzLRYZ0', 'rJTUB-u8U2Y', 'A7eoKD5m6Ek', 'snRi_JET1bg', '8ycnldvJmuA', 'ftODZr2_V5Q'],
+  kubernetes: ['BsiQrEaF3kU', 'Q4qm1hvVR2A', 'WHonjixQgBY', 'KpSjLQpswW4', '4b-H6ZaoNUE', '_sxPf3tHq3s', '0U-SUJCmKAU', 'VxWyvAO3qb8', 'A4-foXsdQiA', 'C6wLm6NRZW4', 'ctKLD4d146g', 'OnwiaDRj1-k', 'R2zktRqz81U', 'N4nRAfBZ3BE', 'C5HqbzLRYZ0', 'rJTUB-u8U2Y', 'A7eoKD5m6Ek', 'snRi_JET1bg', '8ycnldvJmuA', 'ftODZr2_V5Q'],
   development: [
     'xV2EAL3NAVM',
     '2Ni8zfsxW6o',
