1364b1c5784efe1c114a7afe9fc7f3af334ada3c
Pastel Sketchbook <252934749+pastelsketchbook@users.noreply.github.com>
2026-02-22 12:04:54 -0600

    chore(homepage): update dependencies and add new video entries

--- homepage/src/config/videos.ts
+++ homepage/src/config/videos.ts
@@ -54,8 +54,13 @@ export const VIDEO_CONFIG = {
     'nnL78ZVifZU',
     'tPDFgVAp4c4'
   ],
-  kubernetes: ['ORjARjbukhY', 'rk_3xU9OF-k', 'mYClRFIlsFc', '7eoxSgjwYlM', '5Ztm7JNVa8E', 'P_xUJi_qt-Q', 'okVlu1qseI4', 'eI2DXGISpbk', 'MIZ0ATwu7C0', 'rgY4KTMFOMs', 'BsiQrEaF3kU', 'Q4qm1hvVR2A', 'WHonjixQgBY', 'KpSjLQpswW4', '4b-H6ZaoNUE', '_sxPf3tHq3s', '0U-SUJCmKAU', 'VxWyvAO3qb8', 'A4-foXsdQiA', 'C6wLm6NRZW4', 'ctKLD4d146g', 'OnwiaDRj1-k', 'R2zktRqz81U', 'N4nRAfBZ3BE', 'C5HqbzLRYZ0', 'rJTUB-u8U2Y', 'A7eoKD5m6Ek', 'snRi_JET1bg', '8ycnldvJmuA', 'ftODZr2_V5Q', 'FVdv_oTFyZQ'],
+  kubernetes: ['3hSdKvqPcSE', 'y3WwL48DLYw', 'ORjARjbukhY', 'rk_3xU9OF-k', 'mYClRFIlsFc', '7eoxSgjwYlM', '5Ztm7JNVa8E', 'P_xUJi_qt-Q', 'okVlu1qseI4', 'eI2DXGISpbk', 'MIZ0ATwu7C0', 'rgY4KTMFOMs', 'BsiQrEaF3kU', 'Q4qm1hvVR2A', 'WHonjixQgBY', 'KpSjLQpswW4', '4b-H6ZaoNUE', '_sxPf3tHq3s', '0U-SUJCmKAU', 'VxWyvAO3qb8', 'A4-foXsdQiA', 'C6wLm6NRZW4', 'ctKLD4d146g', 'OnwiaDRj1-k', 'R2zktRqz81U', 'N4nRAfBZ3BE', 'C5HqbzLRYZ0', 'rJTUB-u8U2Y', 'A7eoKD5m6Ek', 'snRi_JET1bg', '8ycnldvJmuA', 'ftODZr2_V5Q', 'FVdv_oTFyZQ'],
   development: [
+    'HiaotGxHTfo',
+    'DazzkNtnzec',
+    'ARWkoc3E8uE',
+    'Th5MMOFQbh8',
+    'SpNfrWmI8iE',
     'H-gkXATx8r0',
     '3ybGkjogcFQ',
     'QBUQvfZJpaM',
@@ -125,7 +130,8 @@ export const VIDEO_CONFIG = {
     'IF5sNQH-01c',
     '2kvYb2pVe5o',
     'TLqdeHlAo3A',
-    'FdmBtPScnQs'
+    'FdmBtPScnQs',
+    'wkbrVBCkx6Q'
   ],
   programming: [
     'T5FjMjTQCBI',
