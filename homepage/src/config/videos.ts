f0b32e12a8dc49318ed0f596d221e1c1bc1caf2d
Pastel Sketchbook <252934749+pastelsketchbook@users.noreply.github.com>
2026-03-01 09:16:36 -0600

    chore(homepage): add melos-rs and modern observability videos

--- homepage/src/config/videos.ts
+++ homepage/src/config/videos.ts
@@ -56,6 +56,8 @@ export const VIDEO_CONFIG = {
   ],
   kubernetes: ['3hSdKvqPcSE', 'y3WwL48DLYw', 'ORjARjbukhY', 'rk_3xU9OF-k', 'mYClRFIlsFc', '7eoxSgjwYlM', '5Ztm7JNVa8E', 'P_xUJi_qt-Q', 'okVlu1qseI4', 'eI2DXGISpbk', 'MIZ0ATwu7C0', 'rgY4KTMFOMs', 'BsiQrEaF3kU', 'Q4qm1hvVR2A', 'WHonjixQgBY', 'KpSjLQpswW4', '4b-H6ZaoNUE', '_sxPf3tHq3s', '0U-SUJCmKAU', 'VxWyvAO3qb8', 'A4-foXsdQiA', 'C6wLm6NRZW4', 'ctKLD4d146g', 'OnwiaDRj1-k', 'R2zktRqz81U', 'N4nRAfBZ3BE', 'C5HqbzLRYZ0', 'rJTUB-u8U2Y', 'A7eoKD5m6Ek', 'snRi_JET1bg', '8ycnldvJmuA', 'ftODZr2_V5Q', 'FVdv_oTFyZQ'],
   development: [
+    'WahgsFhj3W0',
+    'uqZ-mwxGf2c',
     'AcpVuvtSXwI',
     'JeqaHMmSh1s',
     'WMcKFQ200OE',
