bb1de648da4010545f1a00c13dd22cd0dafa0a41
Pastel Sketchbook <252934749+pastelsketchbook@users.noreply.github.com>
2026-03-16 06:02:13 -0500

    chore(homepage): add 7 new video entries and update metadata

--- homepage/src/config/videos.ts
+++ homepage/src/config/videos.ts
@@ -38,7 +38,8 @@ export const VIDEO_CONFIG = {
     'QLcNRIHYmj0',
     'rwNfzJanPmk',
     'rxQvDuohbt8',
-    'kvdBJSRUchA'
+    'kvdBJSRUchA',
+    'oSqbvvcADmI'
   ],
   finance: [
     '_4BC0zHGYTw',
@@ -53,10 +54,15 @@ export const VIDEO_CONFIG = {
     'KBfVy5-M-5k',
     'MDNRiJN7aEg',
     'nnL78ZVifZU',
-    'tPDFgVAp4c4'
+    'tPDFgVAp4c4',
+    '2NoJVXPpcco'
   ],
-  kubernetes: ['F09-7mNt3F4', '1PH7UB24xps', 'Hd767VA7Z-0', 'aiYBPCkvhes', 'Pxh2PcNx9W8', '3hSdKvqPcSE', 'y3WwL48DLYw', 'ORjARjbukhY', 'rk_3xU9OF-k', 'mYClRFIlsFc', '7eoxSgjwYlM', '5Ztm7JNVa8E', 'P_xUJi_qt-Q', 'okVlu1qseI4', 'eI2DXGISpbk', 'MIZ0ATwu7C0', 'rgY4KTMFOMs', 'BsiQrEaF3kU', 'Q4qm1hvVR2A', 'WHonjixQgBY', 'KpSjLQpswW4', '4b-H6ZaoNUE', '_sxPf3tHq3s', '0U-SUJCmKAU', 'VxWyvAO3qb8', 'A4-foXsdQiA', 'C6wLm6NRZW4', 'ctKLD4d146g', 'OnwiaDRj1-k', 'R2zktRqz81U', 'N4nRAfBZ3BE', 'C5HqbzLRYZ0', 'rJTUB-u8U2Y', 'A7eoKD5m6Ek', 'snRi_JET1bg', '8ycnldvJmuA', 'ftODZr2_V5Q', 'FVdv_oTFyZQ', 'u60g2nMtVi4'],
+  kubernetes: ['LUOX5xkSyi0', 'u60g2nMtVi4', 'F09-7mNt3F4', '1PH7UB24xps', 'Hd767VA7Z-0', 'aiYBPCkvhes', 'Pxh2PcNx9W8', '3hSdKvqPcSE', 'y3WwL48DLYw', 'ORjARjbukhY', 'rk_3xU9OF-k', 'mYClRFIlsFc', '7eoxSgjwYlM', '5Ztm7JNVa8E', 'P_xUJi_qt-Q', 'okVlu1qseI4', 'eI2DXGISpbk', 'MIZ0ATwu7C0', 'rgY4KTMFOMs', 'BsiQrEaF3kU', 'Q4qm1hvVR2A', 'WHonjixQgBY', 'KpSjLQpswW4', '4b-H6ZaoNUE', '_sxPf3tHq3s', '0U-SUJCmKAU', 'VxWyvAO3qb8', 'A4-foXsdQiA', 'C6wLm6NRZW4', 'ctKLD4d146g', 'OnwiaDRj1-k', 'R2zktRqz81U', 'N4nRAfBZ3BE', 'C5HqbzLRYZ0', 'rJTUB-u8U2Y', 'A7eoKD5m6Ek', 'snRi_JET1bg', '8ycnldvJmuA', 'ftODZr2_V5Q', 'FVdv_oTFyZQ', 'to1PClyd0YA'],
   development: [
+    'FOkG5ScxU88',
+    'ZBW6YozOu78',
+    'gu-5cim8mpA',
+    'a2kADxV0kBM',
     '2iIi1H9V-Hg',
     'xfOATs8ncLM',
     'DIMW-iHlDxE',
